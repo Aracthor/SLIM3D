dad66cebea6eae307d6a6f7a727afed3595d08b0
Aracthor <pourvivien@hotmail.fr>
2015-09-02 10:26:53 +0900

    Log dev paused, we need a resources branch.

--- debug/srcs/Log.cpp
+++ debug/srcs/Log.cpp
@@ -4,7 +4,7 @@
 // Made by Aracthor
 // 
 // Started on  Mon Aug 31 23:13:05 2015 Aracthor
-// Last Update Wed Sep  2 00:22:56 2015 Aracthor
+// Last Update Wed Sep  2 10:22:01 2015 Aracthor
 //
 
 #include "slim3d/core/system.h"
@@ -34,11 +34,11 @@ Log::~Log()
 {
 }
 
+}
+}
+
 # if IS_COMPUTER(SLIM3D_CORE_SYSTEM)
 #  include "Log/ConsoleLog.cpp"
 # else
 #  error "Log class not created for this system."
 # endif
-
-}
-}
--- debug/srcs/Log/ConsoleLog.cpp
+++ debug/srcs/Log/ConsoleLog.cpp
@@ -4,8 +4,13 @@
 // Made by Aracthor
 // 
 // Started on  Tue Sep  1 23:32:18 2015 Aracthor
-// Last Update Wed Sep  2 00:23:36 2015 Aracthor
+// Last Update Wed Sep  2 10:25:37 2015 Aracthor
 //
+
+namespace slim
+{
+namespace debug
+{
 
 void
 Log::init()
@@ -21,4 +26,7 @@ Log::destroy()
 void
 Log::write(const char* message, int level)
 {
+}
+
+}
 }
