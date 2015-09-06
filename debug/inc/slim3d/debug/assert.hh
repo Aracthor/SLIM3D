83f269834dc9c1dd93798a4662eb6642e7e95765
Aracthor <pourvivien@hotmail.fr>
2015-09-06 12:41:36 +0900

    Usage of assertion in any namespace possible.

--- debug/inc/slim3d/debug/assert.hh
+++ debug/inc/slim3d/debug/assert.hh
@@ -4,7 +4,7 @@
 // Made by Aracthor
 // 
 // Started on  Mon Aug 31 23:19:45 2015 Aracthor
-// Last Update Sun Sep  6 12:34:39 2015 Aracthor
+// Last Update Sun Sep  6 12:41:22 2015 Aracthor
 //
 
 #ifndef SLIM3D_DEBUG_ASSERT_HH_
@@ -13,7 +13,7 @@
 # include "slim3d/debug/AssertionException.hh"
 
 # ifdef _DEBUG
-#  define SLIM3D_DEBUG_ASSERT(condition) if (!(condition)) throw AssertionException(__FILE__, __func__, __LINE__);
+#  define SLIM3D_DEBUG_ASSERT(condition) if (!(condition)) throw slim::debug::AssertionException(__FILE__, __func__, __LINE__);
 # else
 #  define SLIM3D_DEBUG_ASSERT(condition)
 # endif // _DEBUG
