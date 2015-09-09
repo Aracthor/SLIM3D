15767740e8c6563dc46d1d7c5a47c6c101bbf532
Aracthor <pourvivien@hotmail.fr>
2015-09-09 13:58:48 +0900

    Dumper added on logs for object data dump.

--- debug/inc/slim3d/debug/SyscallException.hh
+++ debug/inc/slim3d/debug/SyscallException.hh
@@ -4,7 +4,7 @@
 // Made by Aracthor
 // 
 // Started on  Wed Sep  2 17:19:10 2015 Aracthor
-// Last Update Thu Sep  3 23:00:27 2015 Aracthor
+// Last Update Wed Sep  9 11:38:45 2015 Aracthor
 //
 
 #ifndef SLIM3D_DEBUG_SYSCALL_EXCEPTION_HH_
@@ -13,7 +13,7 @@
 # include "slim3d/debug/Exception.hh"
 
 # define SLIM3D_DEBUG_SYSCALL_CALL(syscall)	if ((syscall) == static_cast<int>(-1)) throw slim::debug::SyscallException("SYSCALL FAIL", __FILE__, __func__, __LINE__);
-# define SLIM3D_DEBUG_SYSCALL_CALL_PTR(syscall)	if ((syscall) == NULL) throw slim::debug::SyscallException("SYSCALL FAIL", __FILE__, __func__, __LINE__);
+# define SLIM3D_DEBUG_SYSCALL_CALL_PTR(syscall)	if ((syscall) == nullptr) throw slim::debug::SyscallException("SYSCALL FAIL", __FILE__, __func__, __LINE__);
 
 namespace slim
 {
